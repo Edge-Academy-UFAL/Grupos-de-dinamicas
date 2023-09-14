4f8ab33a9bc17128e05392bbea7b95adcdc9f3d6
rangel <@rangel-goncalves>
2023-09-14 16:53:28 -0300

    Ideia: verificar se naquele dia o aluno tem pelo menos 6 combinações

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import json
+import copy
+import random
 
+
+class grup:
+    def __int__(self, horario, integrantes):
+        self.horario = horario
+        self.integrantes = []
 
 class Aluno:
     def __init__(self, nome, dias):
@@ -15,7 +22,7 @@ class AlunoNaoIT(Aluno):
         self.Comb = {
             "Segunda": {},"Terca": {}, "Quarta": {}, "Quinta": {}, "Sexta": {},
             }
-
+        self.temGP = 0
 
 def transform_value(value):
     if value == "":
@@ -34,7 +41,22 @@ def find_student_by_name(name, alunos):
             return alunos.pop(i)
     return None
 
+def generate_combinations(dias, interval_hours):
+    combinations = []
+    current_combination = []
 
+    for i, value in enumerate(dias):
+        if value == '1':
+            current_combination.append(str(i))
+            if len(current_combination) == interval_hours:
+                combinations.append(''.join(current_combination))
+                current_combination.pop(0)
+        else:
+            current_combination = []
+
+    return combinations
+
+""""" unused
 def generate_combinations(dias, interval_hours):
     combinations = []
     current_combination = []
@@ -49,7 +71,7 @@ def generate_combinations(dias, interval_hours):
             current_combination = []
 
     return combinations
-
+"""""
 
 with open(r'C:\Users\range\Downloads\dados.json', "r") as json_file:
     data = json.load(json_file)
@@ -74,7 +96,7 @@ for aluno_json in data:
     else:
         it_aluno = Aluno(nome, dias)
 
-alunoT = find_student_by_name("T", alunos)
+#alunoT = find_student_by_name("T", alunos)
 
 alunos.pop(0)
 
@@ -89,10 +111,10 @@ if it_aluno:
         Turmas2HGlobal[dia] = combinations_2h
         Turmas3HGlobal[dia] = combinations_3h
 
-    #print("Turmas2HGlobal:")
-    #print(Turmas2HGlobal)
-    #print("Turmas3HGlobal:")
-    #print(Turmas3HGlobal)
+    print("Turmas2HGlobal:")
+    print(Turmas2HGlobal)
+    print("Turmas3HGlobal:")
+    print(Turmas3HGlobal)
 else:
     print("Aluno IT não encontrado.")
 
@@ -102,52 +124,13 @@ for aluno in alunos:
         combinations_3h = generate_combinations(horarios, interval_hours=3)
         aluno.Turmas2H[dia] = combinations_2h
         aluno.Turmas3H[dia] = combinations_3h
-    #print(aluno.nome)
-    #print("Turmas 2H:")
-    #print(aluno.Turmas2H)
-    #print("Turmas 3H:")
-    #print(aluno.Turmas3H)
+    print(aluno.nome)
+    print("Turmas 2H:")
+    print(aluno.Turmas2H)
+    print("Turmas 3H:")
+    print(aluno.Turmas3H)
 
-alunoT = find_student_by_name("S", alunos)
-if alunoT:
-    print("CombinaçõesT de 2 horas para Segunda-feira:")
-    print(alunoT.Turmas2H['Segunda'])
-
-    print("CombinaçõesT de 3 horas para Segunda-feira:")
-    print(alunoT.Turmas3H["Segunda"])
-else:
-    print("Aluno R não encontrado.")
-"""
-alunoX = find_student_by_name("A", alunos)
-
-if alunoX:
-    print("CombinaçõesX de 2 horas para Segunda-feira:")
-    print(alunoX.Turmas2H['Segunda'])
-
-    print("CombinaçõesX de 3 horas para Segunda-feira:")
-    print(alunoX.Turmas3H["Segunda"])
-else:
-    print("Aluno R não encontrado.")
-
-if alunoT and alunoX:
-    dia_semana = "Segunda"  # vai mudando os dias
-
-    if dia_semana in alunoT.Turmas2H and dia_semana in alunoX.Turmas2H:
-        combinacoes_alunoT = set(alunoT.Turmas3H[dia_semana])
-        combinacoes_alunoX = set(alunoX.Turmas3H[dia_semana])
-
-        if combinacoes_alunoT.intersection(combinacoes_alunoX):
-            print("Os alunos têm combinações em comum na Segunda-feira.")
-            print(combinacoes_alunoT.intersection(combinacoes_alunoX))
-        else:
-            print("Os alunos não têm combinações em comum na Segunda-feira.")
-            print(combinacoes_alunoT.intersection(combinacoes_alunoX))
-    else:
-        print("Dia da semana não encontrado para um dos alunos.")
-else:
-    print("Aluno T ou aluno X não encontrado.")
-"""
-
+"""""
 for alunoT in alunos:
     #dia_semana = "Segunda"  # Substitua pelo dia desejado
     for dia_semana in diasSemana:
@@ -164,24 +147,98 @@ for alunoT in alunos:
                 else:
                     print(f"O aluno {alunoT.nome} não tem combinações em comum com o aluno {alunoX.nome} na {dia_semana}.")
 
-for aluno in alunos:
+"""
+quant = 20
+gp_s = []
+while len(gp_s) < 2:
+    copyAlunos = list(copy.copy(alunos))
+    random.shuffle(copyAlunos)
+    #for a in copyAlunos:
+        #a.temGP = 0
+    for alunoT in copyAlunos:
+        tan = 0
+        #print(alunoT.nome)
+        for dia_semana in diasSemana:
+
+                    # print(f'conbinação atual {combinacoes_alunoT} ---- {gp}-----{dia_semana} ---- tan{tan}')
+
+                    #if tan == 7:
+                        #continue
+
+            gp= ''
+            #gp = alunoT.nome
+            tan = 0
+            combinacoes_alunoT = set(alunoT.Turmas3H[dia_semana])
+            combinacoes_alunoTG = set(alunoT.Turmas3H[dia_semana])
+            for alunoX in copyAlunos:
+                if tan >= 7 or alunoX.temGP == 1:
+                    continue
+                if alunoX != alunoT and dia_semana in alunoX.Turmas2H:
+                    combinacoes_alunoX = set(alunoX.Turmas3H[dia_semana])
+
+                    if combinacoes_alunoT.intersection(combinacoes_alunoX) and combinacoes_alunoTG.intersection(combinacoes_alunoX) and tan < 8:
+                        print(f"O aluno {alunoT.nome} tem combinações em comum com o aluno {alunoX.nome} na {dia_semana}.")
+                        print(combinacoes_alunoTG.intersection(combinacoes_alunoX))
+                        tan = tan + 1
+                        alunoX.temGP = 1
+                        #alunoT.temGP = 1
+                        quant -= 1
+                        #print(f' removi {alunoX.nome}')
+                        #copyAlunos.remove(alunoT)
+                        combinacoes_alunoTG = combinacoes_alunoTG.intersection(combinacoes_alunoX)
+                        gp = gp + alunoX.nome
+            if tan >= 6:
+                flag = 1
+                for g in gp_s:
+                    if g == (gp + ' ' + dia_semana):
+                        flag = 0
+                # gp += alunoT.nome
+                if flag == 1:
+                    alunoT.temGP = 1
+
+                    print(alunoT.nome)
+
+                    gp_s.append(gp + ' ' + dia_semana)
+
+                    print(gp)
+                    print(len(gp_s))
+                        #find_student_by_name(alunoX.nome, copyAlunos)
+                        #copyAlunos.remove(alunoX)
+                        #print(f'{alunoT.nome} e {alunoX.nome} : {alunoT.Comb[dia_semana][alunoX.nome]}')
+
+                    #else:
+                        #print(f"O aluno {alunoT.nome} não tem combinações em comum com o aluno {alunoX.nome} na {dia_semana}.")
+    print(gp_s)
+    print(f'restam {quant}')
+def normalize_string(s):
+    # Normaliza a string ordenando os caracteres em ordem alfabética
+    return ''.join(sorted(s))
+
+valores_unicos = set()
+
+for s in gp_s:
+    s_normalizada = normalize_string(s)
+    valores_unicos.add(s_normalizada)
+
+valores_unicos_lista = list(valores_unicos)
+
+print(valores_unicos_lista)
+"""
+for alunoi in copyAlunos:
     for dia in diasSemana:
-        print(f'{aluno.nome} in {dia}:{len(aluno.Comb[dia])} on : {list(aluno.Comb[dia].keys())}')
-
-
-"""def sortedA(alunos, gpTam):
-    if len(alunos) == 0:
-        return
-    if gpTam == 7:
-        return
-    for aluno in alunos:
-        for dia in diasSemana:
-            if len(aluno.Comb[dia]) > 0:
-                keys = list(aluno.Comb[dia].keys())
-                #confirmar se tem uma interceção em comum
-
-
-sortedA(alunos,0)"""
-"""alunoX = find_student_by_name("A", alunos)
-e = set(alunoX.Comb["Terca"]['E'])
-print(list(e)[1])"""
+        for alunoj in copyAlunos:
+            if alunoj== alunoi:
+                continue
+            diaSemanaj = set(alunoi.Comb[dia][alunoj.nome])
+            diaSemanaj = list(diaSemanaj)
+            for hora in diaSemanaj:
+                for
+"""
+"""""
+alunoX = copyAlunos.pop(0)
+quarta = set(alunoX.Comb['Quarta']['M'])
+quarta = list(quarta)
+print(quarta[1])
+#O aluno A tem combinações em comum com o aluno M na Quarta.
+#A e M : {'123', '012'}
+"""
